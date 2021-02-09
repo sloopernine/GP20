7c1daaa2699a8630bfed053ccfbdc5605f6a449e
Robin Bono <robin@omnick.se>
2021-02-09 11:25:12 +0100

    part commit

---
+++ Game Programming/Unity/SaveGame/Assets/Scripts/Tools.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tools : MonoBehaviour
+{
+        public static float WrapAngle(float angle)
+        {
+            angle%=360;
+            if(angle >180)
+                return angle - 360;
+ 
+            return angle;
+        }
+ 
+        // public static float UnwrapAngle(float angle)
+        // {
+        //     if(angle >=0)
+        //         return angle;
+        //
+        //     angle = -angle%360;
+        //
+        //     return 360-angle;
+        // }
+}
